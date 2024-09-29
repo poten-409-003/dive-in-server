af479e1875b7dc9225409614b2bd1ebfe315dcae
Jangeunhye <dmsgp220@gmail.com>
2024-09-29 22:42:54 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/poten/dive_in/DiveInApplication.java
@@ -0,0 +1,13 @@
+package com.poten.dive_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poten/dive_in/DiveInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poten.dive_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
