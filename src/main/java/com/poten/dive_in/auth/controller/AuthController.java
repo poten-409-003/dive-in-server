68488e3cfff220221cea599f76f92c3e33df77a6
Jangeunhye <dmsgp220@gmail.com>
2024-10-05 18:09:02 +0900

    feat: 카카오 로그인

--- src/main/java/com/poten/dive_in/auth/controller/AuthController.java
+++ src/main/java/com/poten/dive_in/auth/controller/AuthController.java
@@ -1,34 +1,51 @@
 package com.poten.dive_in.auth.controller;
 
+import com.poten.dive_in.auth.dto.KakaoAccountDto;
+import com.poten.dive_in.auth.dto.KakaoTokenDto;
+import com.poten.dive_in.auth.dto.UserInfoResponseDto;
 import com.poten.dive_in.auth.service.AuthService;
+import com.poten.dive_in.auth.service.KakaoService;
 import com.poten.dive_in.common.dto.CommonResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
 
     private final AuthService authService;
+    private final KakaoService kakaoService;
 
-    @GetMapping("/auth/login")
-    public ResponseEntity<CommonResponse<Object>> login(@RequestParam String email,@RequestParam String nickName, HttpServletResponse response){
+    @GetMapping("/login/kakao")
+    public ResponseEntity<CommonResponse<Object>> login(@RequestParam String code, @RequestParam String redirect_uri){
 
-        authService.login(email,nickName, response);
-        return new ResponseEntity<>(CommonResponse.success(null,null), HttpStatus.OK);
+        // 카카오 API에 액세스 토큰 요청
+        KakaoTokenDto kakaoTokenDto = kakaoService.getKakaoAccessToken(code);
+
+        // 액세스 토큰으로 유저 정보 요청
+        KakaoAccountDto kakaoAccountDto = kakaoService.getKakaoInfo(kakaoTokenDto.getAccessToken());
+
+        UserInfoResponseDto userInfoResponseDto = authService.login(kakaoAccountDto);
+
+        return new ResponseEntity<>(CommonResponse.success(null,userInfoResponseDto), HttpStatus.OK);
     }
 
-    @DeleteMapping("/auth/logout")
+    @DeleteMapping("/logout")
     public ResponseEntity<CommonResponse<Object>> logout(HttpServletRequest request,HttpServletResponse response){
         authService.logOut(request,response);
         return new ResponseEntity<>(CommonResponse.success(null,null), HttpStatus.OK);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<CommonResponse<Object>> getCurrentUserInfo(Principal principal){
+        UserInfoResponseDto userInfoResponseDto = authService.getCurrentUserInfo(principal.getName());
+        return new ResponseEntity<>(CommonResponse.success(null,userInfoResponseDto), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/poten/dive_in/auth/service/KakaoService.java
@@ -0,0 +1,117 @@
+package com.poten.dive_in.auth.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poten.dive_in.auth.dto.KakaoAccountDto;
+import com.poten.dive_in.auth.dto.KakaoTokenDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class KakaoService {
+
+    @Value("${kakao.kakao_rest_api_key}")
+    private String KAKAO_REST_API_KEY;
+
+    @Value("${kakao.kakao_client_secret}")
+    private String KAKAO_CLIENT_SECRET;
+
+    @Value("${kakao.kakao_redirect_uri}")
+    private String KAKAO_REDIRECT_URI;
+
+
+    public KakaoTokenDto getKakaoAccessToken(String code) {
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 객체 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type","authorization_code");
+        params.add("client_id", KAKAO_REST_API_KEY);
+        params.add("redirect_uri",KAKAO_REDIRECT_URI);
+        params.add("code",code);
+        params.add("client_secret",KAKAO_CLIENT_SECRET);
+
+        // 헤더와 바디 합치기 위해 HttpEntity 객체 생성
+        HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest = new HttpEntity<>(params,headers);
+
+        // 카카오로부터 Access token 수신
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // JSON parsing
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoTokenDto kakaoTokenDto = null;
+        try{
+            kakaoTokenDto = objectMapper.readValue(accessTokenResponse.getBody(),KakaoTokenDto.class);
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        return kakaoTokenDto;
+
+    }
+
+    public KakaoAccountDto getKakaoInfo(String kakaoAccessToken){
+
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer "+kakaoAccessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String,String>> accountInfoRequest = new HttpEntity<>(headers);
+
+        // POST 방식으로 API 서버에 요청 보내고, response 받아옴
+        ResponseEntity<String> accountInfoResponse = rt.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                accountInfoRequest,
+                String.class
+        );
+
+        log.info("카카오 서버에서 정상적으로 데이터를 수신했습니다.");
+
+        // JSON Parsing
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoAccountDto kakaoAccountDto = null;
+        try{
+            JsonNode root = objectMapper.readTree(accountInfoResponse.getBody());
+
+            JsonNode kakaoAccountNode = root.path("kakao_account");
+
+            KakaoAccountDto.Profile profile = objectMapper.treeToValue(kakaoAccountNode.path("profile"), KakaoAccountDto.Profile.class);
+            log.info("프로필: "+profile.getNickname());
+
+            String email = kakaoAccountNode.path("email").asText();
+            log.info("이메일: " +email);
+
+
+            // DTO 생성
+            kakaoAccountDto = KakaoAccountDto.builder()
+                    .email(email)
+                    .profile(profile)
+                    .build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return kakaoAccountDto;
+
+
+    }
+
+}
