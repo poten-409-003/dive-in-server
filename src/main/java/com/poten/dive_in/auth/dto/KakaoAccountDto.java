47444535a62d4d589ff0ddbbdee5b0acba5f2403
Jangeunhye <dmsgp220@gmail.com>
2024-10-05 19:59:54 +0900

    fix: 카카오 dto ignore적용

--- src/main/java/com/poten/dive_in/auth/dto/KakaoAccountDto.java
+++ src/main/java/com/poten/dive_in/auth/dto/KakaoAccountDto.java
@@ -1,5 +1,6 @@
 package com.poten.dive_in.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,12 +9,14 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Builder @AllArgsConstructor @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoAccountDto {
 
     private String email;
 
     @Getter
     @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Profile {
         private String nickname;
 
